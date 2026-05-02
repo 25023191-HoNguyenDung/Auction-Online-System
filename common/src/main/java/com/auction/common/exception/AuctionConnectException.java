f080095384a0247bdec66e562b04840a973d055f
Khanh <zzk3vzz@gmail.com>
2026-05-02 23:31:05 +0700

    them exception

---
+++ common/src/main/java/com/auction/common/exception/AuctionConnectException.java
@@ -0,0 +1,5 @@
+package com.auction.common.exception;
+
+public class AuctionConnectException {
+
+}
---
+++ common/src/main/java/com/auction/common/exception/AuctionTimeException.java
@@ -0,0 +1,7 @@
+package com.auction.common.exception;
+
+public class AuctionTimeException extends Exception {
+    public AuctionTimeException(String message) {
+        super(message);
+    }
+}
