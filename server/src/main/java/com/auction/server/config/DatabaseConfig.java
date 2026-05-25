ece6f9763d05dacb27b1224212d3a0bb480a4900
Bach <bachpvb1234@gmail.com>
2026-05-25 21:57:21 +0700

    fix loi

--- server/src/main/java/com/auction/server/config/DatabaseConfig.java
+++ server/src/main/java/com/auction/server/config/DatabaseConfig.java
@@ -23,7 +23,7 @@ public class DatabaseConfig {
         String password = System.getenv("DB_PASSWORD");
 
         // Sửa 26.134.195.52 thành localhost
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/auction_db?useSSL=false&serverTimezone=UTC");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/auction_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
         config.setUsername("root");
         config.setPassword("@Ducanh2007");
         config.setMaximumPoolSize(10);
